af8ca7c9a4c279730e2b8965cb50c017706adac6
Angelina <midnightshout03@gmail.com>
2024-05-09 12:31:15 +0300

    3

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,25 @@
+import { FaUser } from "react-icons/fa";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import css from './Contact.module.css';
+import { useDispatch } from "react-redux";
+import { deleteContact } from "../../redux/contactsSlice";
+
+export default function Contact({ contact: { name, number, id }}) {
+    const dispatch = useDispatch();
+
+    // const handleDelete = () => {
+    //     dispatch(deleteContact(id))
+    // };
+
+    const handleDelete = () => dispatch(deleteContact(id));
+ 
+    return (
+        <>
+           
+            <p className={css.ctext}> <FaUser /> {name}</p>
+            <p className={css.ctext}> <BsFillTelephoneFill /> {number}</p>
+            <button onClick={handleDelete}>
+            Delete</button>
+        </>
+    )
+}
